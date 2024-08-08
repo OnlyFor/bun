cfe4a5a1d07c6b427653fbbaf1c1ee51635761b4
Ciro Spaciari <ciro.spaciari@gmail.com>
2024-08-07 20:09:53 -0700

    tests

--- test/js/web/fetch/fetch.tls.test.ts
+++ test/js/web/fetch/fetch.tls.test.ts
@@ -258,3 +258,30 @@ it("fetch timeout works on tls", async () => {
     expect(total).toBeLessThanOrEqual(TIMEOUT + THRESHOLD);
   }
 });
+
+it("fetch should abort as soon as possible under tls", async () => {
+  using server = Bun.serve({
+    port: 0,
+    tls: CERT_LOCALHOST_IP,
+    async fetch() {
+      await Bun.sleep(1000);
+      return new Response("Hello World");
+    },
+  });
+  for (const timeout of [0, 1, 10, 20, 100, 300]) {
+    const time = Date.now();
+    try {
+      await fetch(server.url, {
+        //@ts-ignore
+        tls: { ca: CERT_LOCALHOST_IP.cert },
+        signal: AbortSignal.timeout(timeout),
+      }).then(res => res.text());
+    } catch (err) {
+      expect((err as Error).name).toBe("TimeoutError");
+    } finally {
+      const diff = Date.now() - time;
+      expect(diff).toBeLessThanOrEqual(timeout + 10);
+      expect(diff).toBeGreaterThanOrEqual(timeout);
+    }
+  }
+});
